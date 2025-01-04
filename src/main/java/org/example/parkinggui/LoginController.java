8f794291f946cfccdc5599ab93446f55db53cae0
Kiryl Baranouski <kirlanby@gmail.com>
2025-01-04 17:07:43 +0100

    dodane dodatkowe kontrollery, lekko zrobione klasy

---
+++ src/main/java/org/example/parkinggui/LoginController.java
@@ -0,0 +1,7 @@
+package org.example.parkinggui;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Label;
+
+public class LoginController {
+}
--- src/main/java/org/example/parkinggui/symulator/Samochod.java
+++ src/main/java/org/example/parkinggui/symulator/Samochod.java
@@ -1,4 +1,59 @@
 package org.example.parkinggui.symulator;
 
 public class Samochod {
+    String nrRejestracyjny;
+    double rachunek;
+    double dlug;
+    double timeRemaining;
+
+    int nrPietra;
+    int nrRzedu;
+
+    public String getNrRejestracyjny() {
+        return nrRejestracyjny;
+    }
+
+    public double getRachunek() {
+        return rachunek;
+    }
+
+    public double getDlug() {
+        return dlug;
+    }
+
+    public void setDlug(double dlug) {
+        this.dlug = this.dlug + dlug;
+    }
+
+    public double getTimeRemaining() {
+        return timeRemaining;
+    }
+
+    public void setTimeRemaining(double timeRemaining) {
+        this.timeRemaining = timeRemaining;
+    }
+
+    public int getNrPietra() {
+        return nrPietra;
+    }
+
+    public void setNrPietra(int nrPietra) {
+        this.nrPietra = nrPietra;
+    }
+
+    public void resetNrPietra() {
+        this.nrPietra = Integer.parseInt(null);
+    }
+
+    public int getNrRzedu() {
+        return nrRzedu;
+    }
+
+    public void setNrRzedu(int nrRzedu) {
+        this.nrRzedu = nrRzedu;
+    }
+
+    public void resetNrRzedu() {
+        this.nrRzedu = Integer.parseInt(null);
+    }
 }
